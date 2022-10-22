0db78ac09643cbb0ea01af4468d6ffd69cf281cb
Caue Moyano <cauegmoyano@gmail.com>
2022-10-22 18:38:05 -0400

    created help page

--- pages/ajuda.tsx
+++ pages/ajuda.tsx
@@ -1,6 +1,38 @@
-import { Container, Heading } from "@chakra-ui/react";
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+  Container,
+  Heading,
+} from "@chakra-ui/react";
 import React from "react";
 import { NAV_PAGE_PADDING } from "../utilities/constants";
+
+type TQuestion = {
+  question: string;
+  answer: string;
+};
+
+const questionsAndAnswers: TQuestion[] = [
+  {
+    question: "Question 1",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    question: "Question 2",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    question: "Question 3",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+];
 
 const Ajuda = () => {
   return (
@@ -16,12 +48,33 @@ const Ajuda = () => {
         as="h1"
         textAlign="center"
         fontSize={{ base: "4xl", md: "5xl" }}
-        mb={{ base: 4, md: 8 }}
+        mb={{ base: 8, md: 16 }}
         mt={8}
       >
-        Ajuda
+        Perguntas Frequentes
       </Heading>
+      <Accordion width="min(100%, 600px)" mx="auto">
+        {questionsAndAnswers.map((question, i) => (
+          <QuestionItem key={i} {...question} />
+        ))}
+      </Accordion>
     </Container>
+  );
+};
+
+const QuestionItem = ({ question, answer }: TQuestion) => {
+  return (
+    <AccordionItem>
+      <h2>
+        <AccordionButton>
+          <Box fontSize="2xl" color="primary.dark" flex="1" textAlign="left">
+            {question}
+          </Box>
+          <AccordionIcon />
+        </AccordionButton>
+      </h2>
+      <AccordionPanel pb={4}>{answer}</AccordionPanel>
+    </AccordionItem>
   );
 };
 
